a20c25abb80c8d7bdb4e8414f07de5ecf4437901
Parth Soni <parthsoni@gmail.com>
2024-12-30 17:09:58 +0530

    remove home route

--- app.js
+++ app.js
@@ -101,9 +101,6 @@ app.use("/listings" , listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
-app.get("/", (req, res) => {
-  res.send("hotel Booking site")
-})
 
 app.all("*", (req, res, next) => {
   next(new ExpressError(404, "Page not Found!"));
